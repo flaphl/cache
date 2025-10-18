49d8e9fb0bb46f9fa903926328aea948135434c2
dueollo <your-email@domain.com>
2025-10-17 22:15:26 -0400

    Add Flaphl package headers to all cache implementation files
    - Added standard Flaphl package header with copyright notice
    - Includes LICENSE reference for all PHP files
    - Maintains consistency with Flaphl framework standards
    - Updated 15 core implementation files with proper attribution

--- Adapter/TagAwareAdapter.php
+++ Adapter/TagAwareAdapter.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Flaphl package.
+ * 
+ * (c) Jade Phyressi <jade@flaphl.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Flaphl\Element\Cache\Adapter;
 
 use Flaphl\Contracts\Cache\ItemInterface;
